1552abea9fd2cbdacb3307877891884be2ef4023
Jr Bytes <itamazor@gmail.com>
2020-11-05 18:31:25 -0300

    Printed second line without spaces excess.

--- get-quote.py
+++ get-quote.py
@@ -9,8 +9,9 @@ def jrbytes():
 
   last = 13
   rnd = random.randint(0, last)
+  rnd2 = random.randint(0, last)
 
-  print(quotes[rnd])
+  print(quotes[rnd], quotes[rnd2], sep='', end='')
 
 if __name__== "__main__":
   jrbytes()
